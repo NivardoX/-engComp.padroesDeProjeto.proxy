38a8e41e20b276cc9928087801ca0fe0310026ca
Nivardo Albuquerque <nivardo00@gmail.com>
2021-05-21 20:27:32 -0300

    Add files via upload

---
+++ ATMMachine.java
@@ -0,0 +1,12 @@
+public class ATMMachine implements GetATMData{
+	
+	
+	public ATMState getYesCardState() { return hasCard; }
+	public ATMState getNoCardState() { return noCard; }
+	public ATMState getHasPin() { return hasCorrectPin; }
+	public ATMState getNoCashState() { return atmOutOfMoney; }
+	
+		
+	public ATMState getATMState() { return atmState; }
+	public int getCashInMachine() { return cashInMachine; }
+}
